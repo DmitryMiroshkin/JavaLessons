b98490572791af8c0d14af145769d93cc4b04209
DmitryMiroshkin <miroshkindv1999@mail.ru>
2025-11-19 00:38:06 +0300

    Initial commit

---
+++ src/lesson1/Family.java
@@ -0,0 +1,12 @@
+package lesson1;
+
+public class Family {
+    static void main(String[] args) {
+        System.out.println("Olga");
+        System.out.println("Valery");
+        System.out.println("Sergey");
+        System.out.println("Dmitry");
+
+
+    }
+}
---
+++ src/lesson1/HelloWorld.java
@@ -0,0 +1,11 @@
+package lesson1;
+
+public class HelloWorld {
+
+    static void main(String[] args) {
+        System.out.println("Olga");
+        System.out.println("Olga");
+        System.out.println("Olga");
+        System.out.println("Olga");
+    }
+}
