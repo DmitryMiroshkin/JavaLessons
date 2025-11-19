771ba86658185450b514defb5322778cb63a125d
DmitryMiroshkin <miroshkindv1999@mail.ru>
2025-11-20 00:35:54 +0300

    Lesson2

---
+++ src/lessons3/ConditionOperator.java
@@ -0,0 +1,14 @@
+package lessons3;
+
+public class ConditionOperator {
+    static void main(String[] args) {
+        int money = 13;
+     if (money > 10) {
+         System.out.println("pizza");
+     }   else if (money < 6) {
+         System.out.println("sandwich");
+     } else {
+         System.out.println("burger");
+     }
+    }
+}
