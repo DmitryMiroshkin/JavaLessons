11e5317de18d8530212130b32ad3cb403438de4b
DmitryMiroshkin <miroshkindv1999@mail.ru>
2025-11-21 01:14:43 +0300

    Lesson6

---
+++ src/Lessons6/Char.java
@@ -0,0 +1,11 @@
+package Lessons6;
+
+public class Char {
+    static void main() {
+        char x = 88;
+        char x1 = 'x';
+        x1++;
+        System.out.println("Value of x is " + x);
+        System.out.println("Value of x1 is " + x1);
+    }
+}
---
+++ src/Lessons6/FamilyAges.java
@@ -0,0 +1,16 @@
+package Lessons6;
+
+public class FamilyAges {
+    static void main(String[] args) {
+        int Olga = 58;
+        int Valery = 56;
+        int Sergey = 34;
+        int Dmitry = 26;
+        System.out.println("Olga: " + Olga +"!");
+        System.out.println("Valery: " + Valery +"!");
+        System.out.println("Sergey: " + Sergey + "!");
+        System.out.println("Dmitry: " + Dmitry + "!");
+
+
+    }
+}
