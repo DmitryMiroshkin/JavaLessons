7e834efa4df15126a321fb7656722354213a7995
DmitryMiroshkin <miroshkindv1999@mail.ru>
2025-11-21 00:57:27 +0300

    Lesson5

---
+++ src/Lessons5/FloatDouble.java
@@ -0,0 +1,22 @@
+package Lessons5;
+
+public class FloatDouble {
+
+    static void main() {
+//        float radius = 10.8f;
+//        float pi = 3.14f;
+//        float area = pi * radius * radius;
+//        System.out.println(area);
+        int valery = 56;
+        int olga = 58;
+        int sergey = 34;
+        int dmitry = 26;
+        double sum = valery + olga + sergey + dmitry;
+        double average = sum / 4;
+        System.out.println(average);
+
+    }
+}
+
+
+
