4fe04421647d3a076ddd8bee242b52ae26c8fa37
DmitryMiroshkin <miroshkindv1999@mail.ru>
2025-11-21 00:13:53 +0300

    Lesson4

---
+++ src/Lessons4/Integertypes.java
@@ -0,0 +1,20 @@
+package Lessons4;
+
+public class Integertypes {
+    static void main(String[] args) {
+//
+//long speed = 300_000;
+//long distance = 365 * 24 * 60 * 60 * speed;
+//        System.out.println(distance);
+//
+//        byte b = 10;
+//        b = (byte) (b + 10);
+//        b++;
+//        b++;
+//        System.out.println(b);
+int a = 1;
+byte b = (byte) a;
+
+    }
+    }
+
--- src/lessons3/ConditionOperator.java
+++ src/lessons3/ConditionOperator.java
@@ -3,12 +3,12 @@ package lessons3;
 public class ConditionOperator {
     static void main(String[] args) {
         int money = 13;
-     if (money > 10) {
-         System.out.println("pizza");
-     }   else if (money < 6) {
-         System.out.println("sandwich");
-     } else {
-         System.out.println("burger");
-     }
+        if (money > 10) {
+            System.out.println("pizza");
+        } else if (money < 6) {
+            System.out.println("sandwich");
+        } else {
+            System.out.println("burger");
+        }
     }
 }
